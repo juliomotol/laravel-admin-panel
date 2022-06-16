0087468c781f6f801e1e9d5cca6a3bb82ed9b7b9
Julio <julio.motol89@gmail.com>
2022-06-17 02:40:05 +0800

    🐛 Fix css name
    Signed-off-by: Julio <julio.motol89@gmail.com>

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,6 +3,6 @@ let mix = require('laravel-mix');
 
 mix.setPublicPath('resources/dist')
     .setResourceRoot('../') // Turns assets paths in css relative to css file
-    .sass('assets/sass/index.scss', 'css/index.scss')
+    .sass('assets/sass/index.scss', 'css/index.css')
     .js(['node_modules/@coreui/coreui/dist/js/coreui.bundle.js', 'node_modules/simplebar/dist/simplebar.min.js'], 'js/index.js')
     .sourceMaps();
