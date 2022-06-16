d915018bd9bfc529ffc1877e433a31da826532f5
Julio <julio.motol89@gmail.com>
2022-06-17 02:33:05 +0800

    🐛 Fix syntax on `registerBladeDirectives()`
    Signed-off-by: Julio <julio.motol89@gmail.com>

--- src/AdminPanelServiceProvider.php
+++ src/AdminPanelServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace JulioMotol\AdminPanel;
 
+use Composer\InstalledVersions;
 use Illuminate\Support\Facades\Blade;
 use JulioMotol\AdminPanel\Commands\InstallCommand;
 use JulioMotol\AdminPanel\Views\Component;
@@ -44,7 +45,7 @@ class AdminPanelServiceProvider extends PackageServiceProvider
         Blade::directive('adminPanelStyle', function () use ($version_hash) {
             $path = asset('vendor/admin-panel/css/index.css');
 
-            return "<link rel=\"stylesheet\" href=\"$path?$version_hash\">"
+            return "<link rel=\"stylesheet\" href=\"$path?$version_hash\">";
         });
 
         Blade::directive('adminPanelScript', function () use ($version_hash) {
