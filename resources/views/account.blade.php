b4b1744106159ede7df7fc4f769974402afa3cfb
Julio <julio.motol89@gmail.com>
2022-06-16 23:43:35 +0800

    💄 account template updates
    - Add default user icon
    - auto margin right
    - don't show dropdown when no items/groups available
    
    Signed-off-by: Julio <julio.motol89@gmail.com>

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -1,18 +1,20 @@
-<ul class="header-nav ms-3">
+<ul class="header-nav ms-auto">
     <li class="nav-item dropdown">
         <a class="nav-link py-0" data-coreui-toggle="dropdown" href="#" role="button" aria-haspopup="true"
             aria-expanded="false">
             <div class="avatar avatar-md">
-                <img class="avatar-img" src="{{ AdminPanel::resolveAccountAvatar() ?? '' }}" alt="user avatar"> {{-- TODO: add default icon --}}
+                <img class="avatar-img" src="{{ AdminPanel::resolveAccountAvatar() ?? asset('vendor/admin-panel/img/user.png') }}" alt="user avatar">
+                {{-- TODO: add default icon --}}
             </div>
         </a>
-        <div class="dropdown-menu dropdown-menu-end pt-0">
-            @foreach (AdminPanel::account()->items() as $item)
-                @include('admin-panel::partials.account-item', compact('item'))
-            @endforeach
-            @foreach (AdminPanel::account()->groups() as $group)
-                @include('admin-panel::partials.account-group', compact('group'))
-            @endforeach
-        </div>
+        @if (count(AdminPanel::account()->items()) > 0 || count(AdminPanel::account()->groups()) > 0)
+            <div class="dropdown-menu dropdown-menu-end pt-0">
+                @foreach (AdminPanel::account()->items() as $item)
+                    @include('admin-panel::partials.account-item', compact('item'))
+                @endforeach
+                @foreach (AdminPanel::account()->groups() as $group)
+                    @include('admin-panel::partials.account-group', compact('group'))
+                @endforeach
+            </div>
     </li>
 </ul>
