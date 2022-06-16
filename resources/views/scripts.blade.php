be9629803f63f7d8306455d596859c99a0b9018f
Julio <julio.motol89@gmail.com>
2022-06-17 02:25:21 +0800

    📦 Deliver assets via `npm` with build steps
    Signed-off-by: Julio <julio.motol89@gmail.com>

--- resources/views/scripts.blade.php
+++ resources/views/scripts.blade.php
@@ -1,2 +1,1 @@
-<script src="{{ asset('vendor/admin-panel/js/coreui.bundle.min.js') }}?ver={{ version_hash }}"></script>
-<script src="{{ asset('vendor/admin-panel/js/simplebar.min.js') }}?ver={{ version_hash }}"></script>
+<script src="{{ asset('vendor/admin-panel/js/index.js') }}?{{ version_hash }}"></script>
--- resources/views/styles.blade.php
+++ resources/views/styles.blade.php
@@ -1,3 +1,1 @@
-<link rel="stylesheet" href="{{ asset('vendor/admin-panel/css/style.min.css') }}?ver={{ version_hash }}">
-<link rel="stylesheet" href="{{ asset('vendor/admin-panel/css/simplebar.css') }}?ver={{ version_hash }}">
-<link rel="stylesheet" href="{{ asset('vendor/admin-panel/css/simplebar-extended.css') }}?ver={{ version_hash }}">
+<link rel="stylesheet" href="{{ asset('vendor/admin-panel/css/index.css') }}?ver={{ version_hash }}">
