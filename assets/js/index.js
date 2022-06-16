8dec8244ad1420a47d2621bc82683fa645d2b4c5
Julio <julio.motol89@gmail.com>
2022-06-17 03:09:36 +0800

    ⚗️ 📦 Update js compilation strategy
    Signed-off-by: Julio <julio.motol89@gmail.com>

---
+++ assets/js/index.js
@@ -0,0 +1,2 @@
+import '@coreui/coreui';
+import 'simplebar';
--- webpack.mix.js
+++ webpack.mix.js
@@ -4,5 +4,5 @@ let mix = require('laravel-mix');
 mix.setPublicPath('resources/dist')
     .setResourceRoot('../') // Turns assets paths in css relative to css file
     .sass('assets/sass/index.scss', 'css/index.css')
-    .js(['node_modules/@coreui/coreui/dist/js/coreui.bundle.js', 'node_modules/simplebar/dist/simplebar.min.js'], 'js/index.js')
+    .js('assets/js/index.js', 'js/index.js')
     .sourceMaps();
