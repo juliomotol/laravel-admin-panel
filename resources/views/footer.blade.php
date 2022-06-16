a79bef0643b0a00a3d4f3503f5116af4d8b28e52
Julio <julio.motol89@gmail.com>
2022-06-16 23:42:17 +0800

    ✨ Slottable footer content
    Signed-off-by: Julio <julio.motol89@gmail.com>

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,8 +1,13 @@
 <footer class="footer">
-    <div>
-        &copy; 2022.
-    </div>
-    <div class="ms-auto">
-        🔨 by <a href="https://github.com/juliomotol">Julio Motol</a> and ⚡ by <a href="https://coreui.io">CoreUI </a>
-    </div>
+    @if ($footer)
+        {{ $footer }}
+    @else
+        <div>
+            &copy; 2022.
+        </div>
+        <div class="ms-auto">
+            🔨 by <a href="https://github.com/juliomotol">Julio Motol</a> and 🎨 by <a href="https://coreui.io">CoreUI
+            </a>
+        </div>
+    @endif
 </footer>
