c214b32d659ad861d57713a67115301ac6c14abc
Julio <julio.motol89@gmail.com>
2022-06-21 00:45:02 +0800

    🐛 Fix breadcrumbs
    Signed-off-by: Julio <julio.motol89@gmail.com>

--- resources/views/breadcrumbs.blade.php
+++ resources/views/breadcrumbs.blade.php
@@ -1,10 +1,12 @@
-@if (Breadcrumbs::has())
+@php($breadcrumbs = Breadcrumbs::generate())
+
+@if ($breadcrumbs->isNotEmpty())
     <div class="header-divider"></div>
     <div class="container-fluid">
         <nav aria-label="breadcrumb">
             <ol class="breadcrumb my-0 ms-2">
-                @foreach (Breadcrumbs::current() as $breadcrumb)
-                    @if ($crumb->url() && !$loop->last)
+                @foreach ($breadcrumbs as $breadcrumb)
+                    @if ($breadcrumb->url && !$loop->last)
                         <li class="breadcrumb-item">
                             <a href="{{ $breadcrumb->url }}">
                                 {{ $breadcrumb->title }}
@@ -12,7 +14,7 @@
                         </li>
                     @else
                         <li class="breadcrumb-item active" aria-current="page">
-                            {{ $crumb->title }}
+                            {{ $breadcrumb->title }}
                         </li>
                     @endif
                 @endforeach
